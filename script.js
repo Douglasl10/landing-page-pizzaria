bada95ab3f3d1225a4602a63ec2651b51af3f42d
Douglas <ftsgmr@gmail.com>
2025-06-07 16:30:01 -0300

    add-new-section&functions

--- script.js
+++ script.js
@@ -1,6 +1,7 @@
 const modal = document.querySelector('.modal');
 const masc = document.querySelector('.mascara-end')
 
+
 function showUp(){
     modal.style.left = '50%'
     masc.style.visibility = 'visible'
@@ -10,4 +11,16 @@ function showOff(){
     modal.style.left = '-30%'
     masc.style.visibility = 'hidden'
 }
+
+document.addEventListener('DOMContentLoaded', function(){
+
+    const btn = document.querySelectorAll('.botao-ped');
+    const paragrafo = document.querySelectorAll('.pgf');
+
+    btn.forEach(function (btn) {
+        btn.addEventListener('click', function () {
+            alert('Pedido enviado com sucesso!')
+        })
+    })
+})
 
